290837b68a1cd2edf73c65589e6b9e9bfcef9b30
Dev4mir <amir.labib91@gmail.com>
2018-09-26 15:10:12 +0200

    init login component and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from "./app.component";
+import { LoginComponent } from "./components/login/login.component";
+
+import { RoutingModule } from "./routing/routing.module";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [AppComponent, LoginComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, RoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
